beb096ccfbe8292a5dd0dfec0e5fbd271210167f
Geordon Worley <vadixidav@gmail.com>
2017-02-11 15:41:42 -0500

    add robot

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,10 @@ pub struct Obstacle {
     pub coordinate: [f64; 2],
     pub dimensions: [f64; 2],
 }
+
+/// Describes the robot which will be pathfinding.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Robot {
+    pub coordinate: [f64; 2],
+    pub radius: f64,
+}
