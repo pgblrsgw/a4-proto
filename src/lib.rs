a33e37ca4f8b0b41b8ff7646ef8290ee4d1098ce
Geordon Worley <vadixidav@gmail.com>
2017-02-11 16:14:43 -0500

    update protocol to spec

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,24 @@
 #[macro_use]
 extern crate serde_derive;
 
+/// An arbitrary point.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
+}
+
 /// Describes an obstacle to be added to the pathfinding algorithm.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Obstacle {
-    pub coordinate: [f64; 2],
-    pub dimensions: [f64; 2],
+    pub length: f64,
+    pub width: f64,
+    pub point: Point,
 }
 
 /// Describes the robot which will be pathfinding.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Robot {
-    pub coordinate: [f64; 2],
+    pub point: Point,
     pub radius: f64,
 }
