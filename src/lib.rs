ecb0560ba172e5ad12a7637db7ed463ebbff6af9
Geordon Worley <vadixidav@gmail.com>
2017-02-11 15:39:17 -0500

    change format to be more compact

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,6 @@ extern crate serde_derive;
 /// Describes an obstacle to be added to the pathfinding algorithm.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Obstacle {
-    pub width: f64,
-    pub length: f64,
-    #[serde(rename = "coordinateX")]
-    pub coordinate_x: f64,
-    #[serde(rename = "coordinateY")]
-    pub coordinate_y: f64,
+    pub coordinate: [f64; 2],
+    pub dimensions: [f64; 2],
 }
