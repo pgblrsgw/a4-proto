aaddd70874a019cea4b4605b22fb21d8352eb98b
Geordon Worley <vadixidav@gmail.com>
2017-02-11 15:05:12 -0500

    change protocol to match swaggerhub api spec

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,11 @@
 #[macro_use]
 extern crate serde_derive;
 
-/// Describes the shape of an obstacle.
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum Shape {
-    Circular {
-        /// The radius of the circular shape
-        radius: f64,
-    },
-    Rectangular {
-        /// The [width, height] of the rectangle
-        dimensions: [f64; 2],
-        /// An angle of 0 means the width is along the x-axis and height is along the y-axis.
-        /// A left-handed coordinate system is used and `angle` is in radians.
-        angle: f64,
-    },
-}
-
 /// Describes an obstacle to be added to the pathfinding algorithm.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Obstacle {
-    /// [x, y] position of the obstacle
-    pub location: [f64; 2],
-    /// The shape specification of the obstacle
-    pub shape: Shape,
+    pub width: f64,
+    pub length: f64,
+    pub coordinateX: f64,
+    pub coordinateY: f64,
 }
